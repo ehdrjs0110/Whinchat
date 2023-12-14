7bfafc380078326900ec8928fe7e0bab8935dcfa
ehdrjs0110 <ehdrjs0110@naver.com>
2023-12-15 04:08:01 +0900

    Main3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import Sign from './front/Sign';
-import ChatRoom from './front/ChatRoom';
 import Profile from './front/Profile';
 import Main from './front/Main';
 
@@ -14,7 +13,6 @@ function App() {
     <Router>
         <Routes>
           <Route path="/Sign" element={<Sign />} />
-          <Route path="/ChatRoom" element={<ChatRoom />} />
           <Route path="/Profile" element={<Profile />} />
           <Route path="/Main" element={<Main />} />
         </Routes>
