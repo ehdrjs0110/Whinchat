5a8e24b65be9c9a7c220d26c80a8561e96fb369c
ehdrjs0110 <ehdrjs0110@naver.com>
2024-01-02 03:12:43 +0900

    imgUpload

--- src/back/server.js
+++ src/back/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require("cors");
 const mongoose = require('mongoose');
+const path = require('path');
+const multer = require('multer');
 
 const app = express();
 const port = 4000;
@@ -194,10 +196,7 @@ app.post('/profile',async(req,res) => {  // 프로필 변경
               pr : pr
          }}
         )
-      
 
-      
-  
       console.log('Fetched data:', data);
       
       if(data){ 
@@ -216,6 +215,97 @@ app.post('/profile',async(req,res) => {  // 프로필 변경
     console.error(err);
   }
     
+});
+
+//사진 업로드
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    return cb(null, "/home/ubuntu/project/whinchat/public/proimg")
+  },
+  filename: function(req, file, cb) {
+    return cb(null, `${Date.now()}_${file.originalname}`)
+  },
+  filefilter: function (req, file, cb) {
+    var ext = path.extname(file.originalname);
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+      return cb(new Error("PNG, JPG만 업로드하세요"));
+    }
+    cb(null, true);
+  },
+  limits: {
+    fileSize: 1024 * 1024,
+  },
+});
+
+const upload = multer({
+  storage: storage
+  
+});
+
+// app.get("/upload", function (req, res) {
+//   res.render("Profile.jsx");
+// });
+
+// app.get("/home/ubuntu/project/whinchat/public/proimg/:imgName", function(req, res){
+//   res.sendFile(__dirname + "/home/ubuntu/project/whinchat/public/proimg/" + req.params.imgName);
+// });
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+  // console.log(req.body)
+  console.log(req.file)
+  const updateData = (
+    { $set: {
+      filename : req.file.filename }}
+  );
+  const filterCriteria = { id: 'admin' };
+  
+  //DB
+  const { MongoClient, ServerApiVersion } = require('mongodb');
+  const uri = "mongodb://127.0.0.1:16045";
+
+  const dbName = 'whinchat';  // Corrected typo here
+  const collectionName = 'member';
+
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+  const ExistingData = await collection.findOne(filterCriteria);
+  
+  // console.log(ExistingData);
+  // console.log(ExistingData.filename);
+
+  if(ExistingData && ExistingData.filename){
+    const fs = require('fs');
+    const fpath =  `/home/ubuntu/project/whinchat/public/proimg/${ExistingData.filename}`;
+    // console.log(ExistingData.filename);
+    if(fs.existsSync(fpath)){   // 주어진 파일 경로(path)에 해당하는 파일 또는 디렉토리가 존재하는지 확인
+      fs.unlinkSync(fpath);   // 주어진 파일 경로(path)에 해당하는 파일을 동기적으로 삭제
+      console.log('기존 이미지 파일 삭제');
+    }else{
+      console.log("이미지 삭제 실패");
+    }
+    await collection.updateOne(filterCriteria, updateData);
+    console.log('이미지 업로드');
+    res.send(req.file.filename);
+  } else{
+    await collection.updateOne(filterCriteria, updateData)
+      .then(result => {
+        console.log(`Matched ${result.matchedCount} document(s) and modified ${result.modifiedCount} document(s)`);
+        res.send(req.file.filename);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send("서버 오류");
+      });
+  }
+  
 });
 
 app.post('/logout',async(req,res) => {  // 프로필 변경
--- src/front/Main.jsx
+++ src/front/Main.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../css/Main.css';
 import {Cookies} from 'react-cookie';
+import axios from 'axios';
 
 const cookies = new Cookies();
 
@@ -11,6 +12,9 @@ const Main = () => {
 
     const [callname, setCallname] = useState("");
     const [callpr, setCallpr] = useState("");
+    const [file, setFile] = useState("");
+
+    var fileName = "";
 
     useEffect( () => {
       
@@ -53,23 +57,6 @@ const Main = () => {
     // }
   }, [callname, callpr])
     
-
-    const handleAvatarClick = () => {
-      // If not editing avatar, trigger file input click
-      if (!isEditingAvatar && inputRef.current) {
-        inputRef.current.click();
-      }
-      // Toggle the editing state
-      setIsEditingAvatar(!isEditingAvatar);
-    };
-  
-    const handleImageChange = (event) => {
-      const file = event.target.files[0];
-      // Handle the selected file as needed
-      console.log('Selected image:', file);
-      // Reset state after handling the image if needed
-      setIsEditingAvatar(false);
-    };
     //로그아웃
     const logout = () => {
       cookies.remove('id');
@@ -129,6 +116,45 @@ const Main = () => {
       }    
     };
 
+    //이미지 업로드 변경
+    const upload = () => {
+      const formData = new FormData();
+      formData.append('file', file);
+      // formData.append('file', file);
+      axios.post("http://3.36.66.72:4000/upload", formData)
+      .then((res) => {
+        console.log(res.data);
+        fileName = res.data;
+        console.log(fileName);
+        setUploadedImg({
+          fileName,
+          filePath: `http://3.36.66.72:3000/main/${fileName}`,
+        });
+        alert(fileName);
+      })
+      .catch((err) => {
+        console.error(err);
+        alert('프로필 편집 실패');
+      });
+    }
+    const [uploadedImg, setUploadedImg] = useState({
+        fileName: "",
+        filePath: "",
+    });
+    //아바타 이미지 선택
+    const handleAvatarClick = () => {
+      // If not editing avatar, trigger file input click
+      if (!isEditingAvatar && inputRef.current) {
+        inputRef.current.click();
+      }
+      // Toggle the editing state
+      setIsEditingAvatar(!isEditingAvatar);
+    };
+    const onChange = e => {
+      setFile(e.target.files[0]);
+      //이미지
+      setIsEditingAvatar(false);
+    };
 
     const navigate = useNavigate();
 
@@ -166,29 +192,40 @@ const Main = () => {
           </li>
         </ul>
       </nav>
-              <div class="card-body">
-                    <img class="mainavatar" src="http://bootdey.com/img/Content/avatar/avatar1.png" alt=""  onClick={handleAvatarClick}/>
-                    <input
-                    type="file"
-                    accept="image/*"
-                    onChange={handleImageChange}
-                    style={{ display: 'none' }}
-                    ref={inputRef}
-                  />
+      <div class="card-body">
+        <form class="profile">
+          {uploadedImg ? (
+            <>
+              <img class="mainavatar" src={uploadedImg.filePath} alt="" onClick={handleAvatarClick} />
+                {/* <h3>{uploadedImg.fileName}</h3> */}
+            </>
+          ) : (
+            ""
+          )}
+              <img class="mainavatar" src={process.env.PUBLIC_URL +"/proimg/"+uploadedImg.fileName || "http://bootdey.com/img/Content/avatar/avatar1.png"} alt="Uploaded" onClick={handleAvatarClick} style={{ width: "230px", height: "230px" }} />
+                {/* <h3>{fileName}</h3> */}
+              <input 
+                accept="image/*" 
+                ref={inputRef} 
+                type="file" 
+                style={{ display: 'none' }}
+                onChange={(e) => setFile(e.target.files[0])}/> 
+                <h3>{}</h3>   
+                <button class="btn-btn-primary" type="button" onClick={upload}>프로필 편집</button>
+        </form>
 
-              <div className="main-input-field">
-                  <i className="fas fa-user"></i>
-                  <input type="text" placeholder={ callname || "닉네임을 입력하세요."} id='name' name='name' onChange={handleProfile} required/>
-              </div>
-
-              <div className="main-input-field2">
-                  <i className="fas fa-file"></i>
-                  <input type="text" placeholder={ callpr || "자기소개를 적어주세요." } id='pr' name='pr' onChange={handleProfile} />
-              </div>
-                <button class="btn-btn-primary" type="button" onClick={submitProfile}>제출</button>
-                </div>
-            </div>
+        <div className="main-input-field">
+        <i className="fas fa-user"></i>
+        <input type="text" placeholder={ callname || "닉네임을 입력하세요."} id='name' name='name' onChange={handleProfile} required/>
         </div>
+        <div className="main-input-field2">
+          <i className="fas fa-file"></i>
+          <input type="text" placeholder={ callpr || "자기소개를 적어주세요." } id='pr' name='pr' onChange={handleProfile} />
+        </div>
+        <button class="btn-btn-primary" type="button" onClick={submitProfile}>제출</button>
+      </div>
+    </div>
+  </div>
 </body>
     </>
   );
